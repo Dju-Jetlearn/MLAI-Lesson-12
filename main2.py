18be5c40fb37a136dc7ea406c52dc0b9d82df758
Dju-Jetlearn <pavelalcaraz@gmail.com>
2025-10-18 09:54:56 +0100

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,29 @@
+# A redo of a previous algorithm
+
+import pandas as pd
+import matplotlib.pyplot as matpat
+import seaborn as sans
+
+data = pd.read_csv("C:/Users/diego/JetLearn/ML & AI/Lesson 12/Employee.csv")
+
+print(data.head(20))
+print(data.info())
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+
+data["Education"] = le.fit_transform(data["Education"])
+data["Gender"] = le.fit_transform(data["Gender"])
+data["EverBenched"] = le.fit_transform(data["EverBenched"])
+
+print(data.head())
+print(data.info())
+
+x = data[["Education", "JoiningYear", "PaymentTier", "Age", "Gender", "EverBenched", "ExperienceInCurrentDomain"]]
+y = data["LeaveOrNot"]
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 2)
+
