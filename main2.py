a9d8b47ca90d7b0126231138065190a7c765e43f
Dju-Jetlearn <pavelalcaraz@gmail.com>
2025-10-25 09:31:55 +0100

    main2.py

--- main2.py
+++ main2.py
@@ -1,5 +1,4 @@
 # A redo of a previous algorithm
-
 import pandas as pd
 import matplotlib.pyplot as matpat
 import seaborn as sans
@@ -27,3 +26,23 @@ from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 2)
 
+from sklearn.tree import DecisionTreeClassifier
+
+classify = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+
+classify.fit(x_train, y_train)
+y_predict = classify.predict(x_test)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+neo = confusion_matrix(y_test, y_predict)
+
+sans.heatmap(neo, annot = True, fmt = 'd')
+
+matpat.title("Confusion Matrix")
+matpat.xlabel("Prediction")
+matpat.ylabel("Actuality")
+matpat.show()
+
+acc = accuracy_score(y_test, y_predict)
+print("accuracy =", round((acc * 100), 2), "%")
